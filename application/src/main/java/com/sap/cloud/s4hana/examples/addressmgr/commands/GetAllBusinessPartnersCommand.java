ff45ad6488b4698dc2fb49fc55023b93671f4452
Maciej Dluzen <maciej.dluzen@capgemini.com>
2020-05-18 16:26:17 +0200

    Added authorization service OAuth2.0

--- application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
+++ application/src/main/java/com/sap/cloud/s4hana/examples/addressmgr/commands/GetAllBusinessPartnersCommand.java
@@ -3,47 +3,55 @@ package com.sap.cloud.s4hana.examples.addressmgr.commands;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.sap.cloud.sdk.cloudplatform.cache.CacheKey;
+import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
+import com.sap.cloud.sdk.frameworks.hystrix.HystrixUtil;
 import com.sap.cloud.sdk.s4hana.connectivity.CachingErpCommand;
 import com.sap.cloud.sdk.s4hana.datamodel.odata.helper.Order;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
 import org.slf4j.Logger;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import com.sap.cloud.sdk.cloudplatform.logging.CloudLoggerFactory;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.BusinessPartner;
-import com.sap.cloud.sdk.s4hana.datamodel.odata.services.BusinessPartnerService;
-
-import javax.annotation.Nonnull;
-
 public class GetAllBusinessPartnersCommand extends CachingErpCommand<List<BusinessPartner>> {
     private static final Logger logger = CloudLoggerFactory.getLogger(GetAllBusinessPartnersCommand.class);
 
     private static final String CATEGORY_PERSON = "1";
 
-    private static final Cache<CacheKey, List<BusinessPartner>> cache =
-            CacheBuilder.newBuilder()
-                    .maximumSize(50)
-                    .expireAfterWrite(5, TimeUnit.MINUTES)
-                    .build();
+    private static final Cache<CacheKey, List<BusinessPartner>> cache = CacheBuilder.newBuilder()
+            .maximumSize(50)
+            .expireAfterWrite(5, TimeUnit.MINUTES)
+            .build();
 
     private final BusinessPartnerService service;
 
     public GetAllBusinessPartnersCommand(final BusinessPartnerService service) {
-        super(GetAllBusinessPartnersCommand.class);
+        super(HystrixUtil.getDefaultErpCommandSetter(
+                GetAllBusinessPartnersCommand.class,
+                HystrixUtil.getDefaultErpCommandProperties().withExecutionTimeoutInMilliseconds(10000)));
+
         this.service = service;
     }
 
-    @Nonnull
     @Override
     protected Cache<CacheKey, List<BusinessPartner>> getCache() {
         return cache;
     }
 
     @Override
-    protected CacheKey getCommandCacheKey() {
-        return CacheKey.ofTenantIsolation();
+    protected List<BusinessPartner> runCacheable() throws Exception {
+        final List<BusinessPartner> businessPartners = service
+                .getAllBusinessPartner()
+                .select(BusinessPartner.BUSINESS_PARTNER,
+                        BusinessPartner.LAST_NAME,
+                        BusinessPartner.FIRST_NAME)
+                .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
+                .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
+                .execute();
+
+        return businessPartners;
     }
 
     @Override
@@ -51,14 +59,4 @@ public class GetAllBusinessPartnersCommand extends CachingErpCommand<List<Busine
         logger.warn("Fallback called because of exception:", getExecutionException());
         return Collections.emptyList();
     }
-
-    @Override
-    protected List<BusinessPartner> runCacheable() throws Exception {
-        return service.getAllBusinessPartner()
-                .select(BusinessPartner.BUSINESS_PARTNER, BusinessPartner.LAST_NAME, BusinessPartner.FIRST_NAME)
-                .filter(BusinessPartner.BUSINESS_PARTNER_CATEGORY.eq(CATEGORY_PERSON))
-                .orderBy(BusinessPartner.LAST_NAME, Order.ASC)
-                .execute();
-    }
-
 }
